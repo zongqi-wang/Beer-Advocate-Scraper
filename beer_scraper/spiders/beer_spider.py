b2d85f42fdb167ce8c1b7cfe7fecf56be309038b
Zongqi Wang <wangzongqi@msn.com>
2018-05-09 01:44:02 -0700

    unfinished spider

--- beer_scraper/spiders/beer_spider.py
+++ beer_scraper/spiders/beer_spider.py
@@ -3,9 +3,38 @@ import scrapy
 
 
 class BeerSpiderSpider(scrapy.Spider):
-    name = 'beer_spider'
+    name = 'beer_comments'
     allowed_domains = ['beeradvocate.com']
-    start_urls = ['http://beeradvocate.com/']
 
-    def parse(self, response):
-        pass
+    ###########################################################################
+    # Description: This function gets all the beer
+    def start_requests(self):
+        start_urls = [
+            'http://www.beeradvocate.com/beer/profile/100'
+            'http://www.beeradvocate.com/beer/profile/1'
+        ]
+        #for brewerynumber in range(1,10):
+        #    start_urls.extend(f'http://www.beeradvocate.com/beer/profile/{brewerynumber}')
+
+        for url in start_urls:
+            yield scrapy.Request(url=url, callback=self.parse_beer)
+
+
+    def parse_beer(self, response):
+        urls = response.css('td.hr_bottom_light a').re('"(.*profile.*)"')
+
+        for url in urls:
+            response.follow(url, self.parse_comment)
+
+    
+    def parse_comment(self, response):
+        for comment in response.css('#rating_fullview_content_2'):
+            yield{
+                'brewery_name': response.css('h1').re('> (.*)</s'),
+                'beer_name': response.css('h1').re('>(.*)<s'),
+                'comment': comment.css('#rating_fullview_content_2').extract(),
+            }
+
+        
+
+
