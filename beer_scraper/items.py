97802861297f0dce7f0f355cef78c45a99a2744e
Zongqi Wang <wangzongqi@msn.com>
2018-05-14 10:10:39 -0700

    recommit

---
+++ beer_scraper/items.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CommentItem(scrapy.Item):
+    beer_number = scrapy.Field() 
+    comment = scrapy.Field()    
+
+class BeerInfoItem(scrapy.Item):
+    beer_name = scrapy.Field()
+    beer_number = scrapy.Field() 
+    brewery_name = scrapy.Field()
+    brewery_number = scrapy.Field()
+    style = scrapy.Field()
+    ABV = scrapy.Field()
+    availability = scrapy.Field()
+    notes = scrapy.Field()
